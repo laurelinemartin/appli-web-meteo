4bd206fca59e72d9fb0fa0f28d896508f4abe5d5
Laureline <laureline.martin@hotmail.com>
2024-02-28 15:36:43 +0100

    Le fichier script qui permet de se connecter et de récupérer les données via l'API.

---
+++ correction/script.js
@@ -0,0 +1,50 @@
+function getCity() {
+	var city = document.getElementById("inputVille").value;
+	console.log(city);
+	var url = 'https://api.openweathermap.org/data/2.5/weather?q=' 
+			+ city 
+			+ '&lang=fr&units=metric&mode=JSON&appid=<APIKEY>' // Modifier l'API Key !
+	getCurrentWeatherByCity(url);
+}
+
+function getCurrentWeatherByCity(URL){
+	// console.log(URL);
+	window.fetch(URL)
+			.then(function(res){
+	 			if (res.ok) {
+	 				return res.json();
+	 			}
+	 		})
+	 		.then(function(res){
+	 			screenCurrentWeather(res);
+	 		})
+			.catch(function(err){
+				console.log("oh no !");
+	})
+}
+
+
+function getCurrentWeather(){
+	window.fetch('https://api.openweathermap.org/data/2.5/weather?q=Bordeaux&lang=fr&units=metric&mode=JSON&appid=<APIKEY') // Modifier l'APIKey
+	.then(function(res){
+ 			if (res.ok) {
+ 				return res.json();
+ 			}
+ 		})
+ 		.then(function(res){
+ 			// console.log(res);
+ 			screenCurrentWeather(res);
+ 		})
+ 		.catch(function(err){console.log("oh no...")});
+}
+
+function screenCurrentWeather(weather){
+	console.log("ville : "+weather.name);
+	document.getElementById("temperature")
+ 			.innerText="La température actuelle est de "
+ 			+weather.main.temp
+ 			+"°C "
+ 			+"à "
+ 			+weather.name
+ 			+".";
+}
